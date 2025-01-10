82716de943e6e4ab04cc977e40b18734af5f2e58
Roeich <roeichsheikh@gmail.com>
2025-01-10 21:03:10 +0600

    working on home page

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    /* ---------------------------- start home page ---------------------------- */
+    $(".banner_slider").owlCarousel({
+        items: 1,
+        dots: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 5000
+    });
+    /* ---------------------------- end home page ---------------------------- */
+})
