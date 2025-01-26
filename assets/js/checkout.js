94e3573a9279202923901ec7393faa7dbc4ab03b
Roeich <roeichsheikh@gmail.com>
2025-01-26 23:17:56 +0600

    working on checkout page

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
     /* ----------------- start checkout page ----------------- */
+    // increase and decrease input number 
+    $('.increase').click(function() {
+        var input = $(this).siblings('.num_inp');
+        var currentValue = parseInt(input.val());
+        input.val(currentValue + 1);
+    });
+    $('.decrease').click(function() {
+        var input = $(this).siblings('.num_inp');
+        var currentValue = parseInt(input.val());
+        if (currentValue > 0) {
+            input.val(currentValue - 1);
+        }
+    });
+
     // control payment method modal
     $(".payment_method input").each((ind, elm) => {
         if ($(elm).is(':checked')) {
@@ -14,5 +28,6 @@ $(document).ready(function(){
         $(".payment_with").html(`- Pay with <b>${$(methodBox).text()}</b>`);
         $("#paymentMethod_modal").modal("hide");
     });
+
     /* ----------------- end checkout page ----------------- */
 });
