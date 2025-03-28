a7634f7773269d36e3963fbd8485314746dfee1a
Roeich <roeichsheikh@gmail.com>
2025-03-28 23:56:06 +0600

    updated summary_order.js file

--- assets/js/summary_order.js
+++ assets/js/summary_order.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     // payment countdown
-    $("#payment_countdown").countdown($("#payment_countdown").attr("data-date"), function(event) {
-        var $this = $(this).html(event.strftime(''
-            + '<span>%d</span>d '
-            + '<span>%H</span>:'
-            + '<span>%M</span>:'
-            + '<span>%S</span>')
-        );
+    $("#payment_countdown").countdown($("#payment_countdown").attr("data-date"), function (event) {
+        $(this).html(event.strftime(
+            '<span>%D</span>d ' +  // Use %D for total days across months
+            '<span>%H</span>:' +
+            '<span>%M</span>:' +
+            '<span>%S</span>'
+        ));
     });
+
 })
